9390ae968e654a776ed6acb9048ef4602714d0a7
Jprummel <jordiprummel@live.nl>
2016-12-22 23:01:40 +0100

    Saving and loading, Character creation and stats
    -Created a save and load system
    -Created Character Creation
    -Created playerinformation script which contains the players name, stats
    and more

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class ChangeScene : MonoBehaviour {
+
+    public void LoadScene(string sceneName)
+    {
+        //This function loads a scene with the name of the scene to be loaded as a parameter
+        SceneManager.LoadScene(sceneName);
+    }
+}
