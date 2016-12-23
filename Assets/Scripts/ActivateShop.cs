c14efcbebc74f011c212f0421030ae5cefb0630e
Lorenzo <18927@ma-web.nl>
2016-12-23 10:59:14 +0100

    shot interface

---
+++ Assets/Scripts/ActivateShop.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ActivateShop : MonoBehaviour {
+
+    [SerializeField]private GameObject _panel;
+
+	public void OpenShop()
+    {
+        _panel.SetActive(true);
+    }
+
+    public void CloseShop()
+    {
+        _panel.SetActive(false);
+    }
+}
