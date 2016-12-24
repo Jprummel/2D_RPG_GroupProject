07c908941966ae8db991d19bbdaf985ecb0045b7
NLferdiNL <nlferdinl@gmail.com>
2016-12-24 23:04:59 +0100

    Basic Input

---
+++ Assets/Scripts/Input/InputData.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+
+public class InputData : MonoBehaviour {
+
+    [SerializeField]
+    bool _up, _down, _left, _right, _dodge, _primary, _secondary, _interact;
+
+    public bool up {
+        get { return _up; }
+        set { HandleDoubleInput(value, _down, out _up, out _down); }
+    }
+
+    public bool down {
+        get { return _down; }
+        set { HandleDoubleInput(_up, value, out _up, out _down); }
+    }
+
+    public bool left {
+        get { return _left; }
+        set { HandleDoubleInput(value, _right, out _left, out _right); }
+    }
+
+    public bool right {
+        get { return _right; }
+        set { HandleDoubleInput(_left, value, out _left, out _right); }
+    }
+
+    public  bool dodge {
+        get { return _dodge; }
+        set { _dodge = value; }
+    }
+
+    public bool primary {
+        get { return _primary; }
+        set { HandleDoubleInput(value, _secondary, out _primary, out _secondary); }
+    }
+
+    public bool secondary {
+        get { return _secondary; }
+        set { HandleDoubleInput(_primary, value, out _primary, out _secondary); }
+    }
+
+    public bool interact {
+        get { return _interact; }
+        set { _interact = value; }
+    }
+
+    /// <summary>
+    /// Checks if both inputs is true.
+    /// 
+    /// If false sets toEditA to a
+    /// and sets toEditB to b.
+    /// 
+    /// If true both out values return false.
+    /// </summary>
+    /// <param name="a">in value a</param>
+    /// <param name="b">in value b</param>
+    /// <param name="toEditA">out value a</param>
+    /// <param name="toEditB">out value b</param>
+    void HandleDoubleInput(bool a, bool b, out bool toEditA, out bool toEditB) {
+        if (a && b) {
+            toEditA = toEditB = false;
+        } else {
+            toEditA = a;
+            toEditB = b;
+        }
+    }
+}
---
+++ Assets/Scripts/Input/KeyboardInput.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(InputData))]
+public class KeyboardInput : MonoBehaviour {
+
+    KeyCode _upKeyCode = KeyCode.W;
+    KeyCode _downKeyCode = KeyCode.S;
+    KeyCode _leftKeyCode = KeyCode.A;
+    KeyCode _rightKeyCode = KeyCode.D;
+
+    KeyCode _dodgeKeyCode = KeyCode.Space;
+    KeyCode _interactKeyCode = KeyCode.E;
+
+    InputData inputData;
+
+    //string _primary = "LMB";
+    //string _secondary = "RMB";
+
+    // For custom key binding
+    public KeyCode upKeyCode {
+        set { _upKeyCode = value; }
+    }
+
+    public KeyCode downKeyCode {
+        set { _downKeyCode = value; }
+    }
+
+    public KeyCode leftKeyCode {
+        set { _leftKeyCode = value; }
+    }
+
+    public KeyCode rightKeyCode {
+        set { _rightKeyCode = value; }
+    }
+
+    public KeyCode dodgeKeyCode {
+        set { _dodgeKeyCode = value; }
+    }
+
+    public KeyCode interactKeyCode {
+        set { _interactKeyCode = value; }
+    }
+
+    void Start() {
+        inputData = GetComponent<InputData>();
+    }
+
+    void Update() {
+        inputData.up = Input.GetKey(_upKeyCode);
+        inputData.down = Input.GetKey(_downKeyCode);
+        inputData.left = Input.GetKey(_leftKeyCode);
+        inputData.right = Input.GetKey(_rightKeyCode);
+
+        inputData.dodge = Input.GetKey(_dodgeKeyCode);
+        inputData.interact = Input.GetKeyDown(_interactKeyCode);
+
+        inputData.primary = Input.GetMouseButtonDown(0);
+        inputData.secondary = Input.GetMouseButtonDown(1);
+    }
+}
