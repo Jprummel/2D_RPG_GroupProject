8c07bf202232aec4fa53801c7aa5c1304693f591
Dylan Waij <d_way@live.nl>
2017-01-10 08:52:16 +0100

    Push

---
+++ Assets/Scripts/ControllerInput.cs
@@ -0,0 +1,285 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControllerInput : MonoBehaviour {
+	
+	public int  joystickNumber; //Assigns Controller number to player object
+	//Face Buttons
+	public bool _aPressed;
+	public bool _xPressed;
+	public bool _yPressed;
+	public bool _bPressed;
+	//Start and Back
+	public bool _startPressed;
+	public bool _backPressed;
+	//Bumpers and Triggers
+	public bool _rbPressed;
+	public bool _rtPressed;
+	public bool _lbPressed;
+	public bool _ltPressed;
+	//Analog Clicks
+	public bool _rsPressed;
+	public bool _lsPressed;
+	//Analog Sticks
+	//Left
+	public bool _lsLeft;
+	public bool _lsRight;
+	public bool _lsUp;
+	public bool _lsDown;
+	//Right
+	public bool _rsLeft;
+	public bool _rsRight;
+	public bool _rsUp;
+	public bool _rsDown;
+	//DPAD
+	public bool _dPadUp;
+	public bool _dPadDown;
+	public bool _dPadLeft;
+	public bool _dPadRight;
+	
+	void Start()
+	{
+		
+	}
+	
+	void Update()
+	{
+		XboxInput();
+	}
+	
+	public void XboxInput()
+	{
+		
+		string joystickString = joystickNumber.ToString();
+		
+		if(Input.GetButtonDown(AxisInput.a + joystickString))  //A BUTTON
+		{
+			_aPressed = true;
+		}
+		else
+		{
+			_aPressed = false;
+		}
+		
+		if (Input.GetButtonDown(AxisInput.b + joystickString)) //B BUTTON
+		{
+			_bPressed = true;
+		}
+		else
+		{
+			_bPressed = false;
+		}
+		
+		if (Input.GetButtonDown(AxisInput.x + joystickString)) //X BUTTON
+		{
+			_xPressed = true;
+		}
+		else
+		{
+			_xPressed = false;
+		}
+		
+		if (Input.GetButtonDown(AxisInput.y + joystickString)) //Y BUTTON
+		{
+			_yPressed = true;
+		}
+		else
+		{
+			_yPressed = false;
+		}
+		
+		if (Input.GetButtonDown(AxisInput.lb + joystickString)) //LEFT BUMPER
+		{
+			_lbPressed = true;
+		}
+		else
+		{
+			_lbPressed = false;
+		}
+		
+		if (Input.GetButtonDown(AxisInput.rb + joystickString)) //RIGHT BUMPER
+		{
+			_rbPressed = true;
+		}
+		else
+		{
+			_rbPressed = false;
+		}
+		
+		if (Input.GetButtonDown(AxisInput.back + joystickString)) //BACK BUTTON
+		{
+			_backPressed = true;
+		}
+		else
+		{
+			_backPressed = false;
+		}
+		
+		if (Input.GetButtonDown(AxisInput.start + joystickString)) //START BUTTON
+		{
+			_startPressed = true;
+		}
+		else
+		{
+			_startPressed = false;
+		}
+		
+		if (Input.GetButtonDown(AxisInput.l3 + joystickString)) //L3 CLICK
+		{
+			_lsPressed = true;
+		}
+		else
+		{
+			_lsPressed = false;
+		}
+		
+		if (Input.GetButtonDown(AxisInput.r3 + joystickString)) //R3 CLICK
+		{
+			_rsPressed = true;
+		}
+		else
+		{
+			_rsPressed = false;
+		}
+		
+		if (Input.GetAxis(AxisInput.rt + joystickString)>0) //RIGHT TRIGGER
+		{
+			_rtPressed = true;
+		}
+		else
+		{
+			_rtPressed = false;
+		}
+		
+		if (Input.GetAxis(AxisInput.lt + joystickString)>0) //LEFT TRIGGER
+		{
+			_ltPressed = true;
+			Debug.Log("LT");
+		}
+		else
+		{
+			_ltPressed = false;
+		}
+		
+		//ANALOG STICKS
+		float leftX = Input.GetAxis(AxisInput.leftX + joystickString); //LEFT X
+		
+		if(leftX > 0)
+		{
+			_lsRight = true;
+		}
+		else
+		{
+			_lsRight = false;
+		}
+		
+		if (leftX < 0)
+		{
+			_lsLeft = true;
+		}
+		else
+		{
+			_lsLeft = false;
+		}
+		
+		float leftY = Input.GetAxis(AxisInput.leftY + joystickString); //LEFT Y
+		
+		if(leftY > 0)
+		{
+			_lsDown = true;
+		}
+		else
+		{
+			_lsDown = false;
+		}
+		
+		if(leftY < 0)
+		{
+			_lsUp = true;
+		}else
+		{
+			_lsUp = false;  
+		}
+		
+		float rightX = Input.GetAxis(AxisInput.rightX + joystickString); // RIGHT X
+		
+		if(rightX > 0)
+		{
+			_rsRight = true;
+		}
+		else
+		{
+			_rsRight = false;
+		}
+		
+		if (rightX < 0)
+		{
+			_rsLeft = true;
+		}
+		else
+		{
+			_rsLeft = false;
+		}
+		
+		float rightY = Input.GetAxis(AxisInput.rightY + joystickString); //Right Y
+		
+		if(rightY > 0)
+		{
+			_rsDown = true;
+		}
+		else
+		{
+			_rsDown = false;
+		}
+		
+		if(rightY < 0)
+		{
+			_rsUp = true;
+		}
+		else
+		{
+			_rsUp = false;
+		}
+		
+		//DPAD
+		float dPadX = Input.GetAxis(AxisInput.dpadX ); //Dpad X
+		
+		if(dPadX < 0)
+		{
+			_dPadLeft = true;
+		}
+		else
+		{
+			_dPadLeft = false;
+		}
+		
+		if (dPadX > 0)
+		{
+			_dPadRight = true;
+		}
+		else
+		{
+			_dPadRight = false;
+		}
+		
+		float dPadY = Input.GetAxis(AxisInput.dpadY);
+		
+		if (dPadY < 0)
+		{
+			_dPadDown = true;
+		}
+		else
+		{
+			_dPadDown = false;
+		}
+		
+		if (dPadY > 0)
+		{
+			_dPadUp = true;
+		}
+		else
+		{
+			_dPadDown = false;
+		}
+	}
+}
