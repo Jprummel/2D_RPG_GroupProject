b8af36fa2b504aee336ada99ab5662fc57e26717
Lorenzo <18927@ma-web.nl>
2017-01-11 10:58:57 +0100

    little script

---
+++ Assets/Scripts/Shop/SetItemValues.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SetItemValues : MonoBehaviour {
+
+    private Image _image;
+    private Text _text;
+    [SerializeField]private int _itemId;
+
+	// Use this for initialization
+	void Start () {
+        _image = GetComponentInChildren<Image>();
+        _text = GetComponentInChildren<Text>();
+
+        //itemdatabase.fetchitem(_itemId);
+        //_image.sprite = item.image;
+        //_text.text = item.value;
+	}
+}
